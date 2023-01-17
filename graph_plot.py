fe2fdf96171f2382ba23a8b4f8c5547f1de5aee0
j0ek0177 <kevin.ell@unibw.de>
2023-01-17 12:47:45 +0100

    bug fix graph plot

--- graph_plot.py
+++ graph_plot.py
@@ -9,6 +9,9 @@ ax = fig.add_subplot(111, projection='3d')
 ax.legend()
 ax.set_aspect('equal', adjustable='box')
 
+lim_min = 100000000000
+lim_max = 0
+
 
 def plot(maneuver):
     """
@@ -22,13 +25,19 @@ def plot(maneuver):
 
 
 def __draw_help(maneuver):
+     global lim_max, lim_min
      # translating maneuver to coordinate points
      xs, ys, zs = plot(maneuver)
 
      # setting limitations for the plot
-     lim_min = min(min(xs), min(ys), min(zs))
-     lim_max = max(max(xs), max(ys), max(zs))
+     lim_min_tmp = min(min(xs), min(ys), min(zs))
+     lim_max_tmp = max(max(xs), max(ys), max(zs))
      
+     if lim_min > lim_min_tmp:
+          lim_min = lim_min_tmp
+     if lim_max < lim_max_tmp:
+          lim_max = lim_max_tmp
+          
      ax.set_xlim3d(lim_min, lim_max)
      ax.set_ylim3d(lim_min, lim_max)
      ax.set_zlim3d(lim_min, lim_max)
@@ -41,3 +50,6 @@ def draw_maneuvers(maneuvers: list):
      for m in maneuvers:
           __draw_help(m)
      plt.show()
+     
+     
+draw_maneuvers(parse_file("Looping").generate_maneuvers(5))
